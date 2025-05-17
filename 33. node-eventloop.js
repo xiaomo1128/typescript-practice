f23bfa5ea29e368ca4af23242d6dceb0466fac2b
xianglei <493559026@qq.com>
2025-05-17 14:27:12 +0800

    fix: 33

--- 33. node-eventloop.js
+++ 33. node-eventloop.js
@@ -1,7 +1,7 @@
 
 // setTimeout 和 setImmediate 在主栈中执行时顺序是不一定的
 require("fs").readFile("./note.md", function () {
-  // poll -> check -> timer
+  // i/o回调 -> poll -> check -> timer
   setTimeout(() => {
     // timers
     console.log("setTimeout");
